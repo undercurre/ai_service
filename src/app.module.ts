e969a5cbeb96584ddd228cadbac41eb0a2906353
lirh42 <undercurre@163.com>
2024-10-12 18:21:44 +0800

    feat: 注入数据库和拦截器（ing）

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+     ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [],
+        synchronize: true,
+      }),
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
